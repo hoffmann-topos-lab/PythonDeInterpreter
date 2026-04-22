2ac2c3baf6f972e0f2b9902041c5d848c04b4529
hoffmannSecLab <157518577+hoffmann-topos-lab@users.noreply.github.com>
2026-04-22 19:11:47 -0300

    loader.py
    Signed-off-by: hoffmannSecLab <157518577+hoffmann-topos-lab@users.noreply.github.com>

--- Decompiler/loader.py
+++ Decompiler/loader.py
@@ -5,7 +5,7 @@ from typing import Optional
 
 def load_code_object(pyc_path):
     with open(pyc_path, "rb") as f:
-        f.read(16)  # cabeçalho do .pyc (Python 3.7+)
+        f.read(16)  
         return marshal.load(f)
 
 def _find_code_by_name(co: types.CodeType, name: str) -> Optional[types.CodeType]:
